45177091272f2e4a71049b9c9bad30b99458835d
bashard001 <bdaseh@gmail.com>
2020-01-02 18:04:47 -0800

    working on the application structure

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,14 @@
-const burgers = require("")
+const burger = require("../model/burger");
+const router = express.Router();
+const express = require("express");
+
+router.get("/", function(req, res){
+    burger.all(function(data){
+        var hbsObject = {
+            burger: data
+        };
+        res.render("index", hbsObject)
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express")
-const mysql = require("mysql")
+
 const exphbs = require("express-handlebars");
 
 const app = express()
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json())
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller")
+
+app.use(routes);
 
 
 
